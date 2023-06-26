d5ae85d9cd86d3fa5e0e9b47ba92f79bbe2418d2
efati geovani <127381007+efatigeovaniisir@users.noreply.github.com>
2023-06-26 21:48:34 +0900

    tambah php-html.php

---
+++ php-html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kombinasi PHP & HTML</title>
+</head>
+<body>
+    <h1>Kombinasi PHP & HTML</h1>
+
+    <?php 
+        echo "Hello PHP!"
+    ?>
+</body>
+</html>
