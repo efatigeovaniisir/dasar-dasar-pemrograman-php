28e4153073113213267056df8d91a9900bbb6fea
efati geovani <127381007+efatigeovaniisir@users.noreply.github.com>
2023-06-26 21:59:54 +0900

    tambah comments & syntax

---
+++ comments.php
@@ -0,0 +1,12 @@
+<?php 
+    // contoh komentar single-line di php
+
+    # ini jugga contoh komentar single-line
+
+    /*
+    kalau yang ini merupakan contoh komentar multiple-lineds yang bisa diterapkan di dalam sebuah file php
+    */
+     
+    echo "PHP Comments";
+
+?>
